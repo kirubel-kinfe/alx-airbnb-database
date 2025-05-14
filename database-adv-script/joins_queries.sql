30b60beb7002a9c6283009543822a6346b8278f6
Kirubel Kinfe <kirubelk@act.com.et>
2025-05-14 10:20:34 +0300

    advanced queries added

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,70 @@
+-- INNER JOIN
+
+SELECT 
+    b.id AS booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    u.id AS user_id,
+    u.name AS user_name,
+    u.email
+FROM 
+    bookings b
+INNER JOIN 
+    users u ON b.user_id = u.id;
+
+
+-- LEFT JOIN
+
+SELECT 
+    p.id AS property_id,
+    p.name AS property_name,
+    r.id AS review_id,
+    r.rating,
+    r.comment
+FROM 
+    properties p
+LEFT JOIN 
+    reviews r ON p.id = r.property_id;
+
+
+-- FULL JOIN
+
+SELECT 
+    u.id AS user_id,
+    u.name AS user_name,
+    b.id AS booking_id,
+    b.start_date,
+    b.end_date
+FROM 
+    users u
+FULL OUTER JOIN 
+    bookings b ON u.id = b.user_id;
+
+
+-- UNION JOIN
+
+SELECT 
+    u.id AS user_id,
+    u.name AS user_name,
+    b.id AS booking_id,
+    b.start_date,
+    b.end_date
+FROM 
+    users u
+LEFT JOIN 
+    bookings b ON u.id = b.user_id
+
+UNION
+
+SELECT 
+    u.id AS user_id,
+    u.name AS user_name,
+    b.id AS booking_id,
+    b.start_date,
+    b.end_date
+FROM 
+    users u
+RIGHT JOIN 
+    bookings b ON u.id = b.user_id;
+

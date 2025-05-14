f8ad15e104824dae72ec64082bbd1111066ee00c
Kirubel Kinfe <kirubelk@act.com.et>
2025-05-14 10:50:26 +0300

    subqueries and readme added

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,41 @@
+-- Non-Correlated Subquery
+
+SELECT 
+    property_id,
+    name,
+    description,
+    location,
+    pricepernight
+FROM 
+    Property
+WHERE 
+    property_id IN (
+        SELECT 
+            property_id
+        FROM 
+            Review
+        GROUP BY 
+            property_id
+        HAVING 
+            AVG(rating) > 4.0
+    );
+
+-- Correlated Subquery
+
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM 
+    User u
+WHERE (
+    SELECT 
+        COUNT(*)
+    FROM 
+        Booking b
+    WHERE 
+        b.user_id = u.user_id
+) > 3;
+
+
